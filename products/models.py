d9eeac9ee991e0a3bb51800a6e3873f427e01fd2
Goran Čeko <goran@gekosolutions.hr>
2022-08-31 14:07:04 +0200

    feat(products): Updated models to support custom fields in ManyToMany relation

--- products/models.py
+++ products/models.py
@@ -3,9 +3,20 @@ from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
 
 
-class Product(models.Model):
+class ProductAbstract(models.Model):
+    rating = models.FloatField(validators=[MinValueValidator(0.0), MaxValueValidator(5.0)])
+
+    class Meta:
+        abstract = True
+
+
+class Product(ProductAbstract):
     name = models.CharField(max_length=256, unique=True)
     price = models.DecimalField(decimal_places=2, max_digits=8)
-    rating = models.FloatField(validators=[MinValueValidator(0.0), MaxValueValidator(5.0)])
     updated_at = models.DateTimeField(null=True, auto_now=True)
-    ratings = models.ManyToManyField(User)
+    ratings = models.ManyToManyField(User, through='Rating')
+
+
+class Rating(ProductAbstract):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='rating_set')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='rating_set')
